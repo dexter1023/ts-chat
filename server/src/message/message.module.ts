c73b6787063f72fa54584d5d966fc2e588c39346
Damian Szczepański <damian@MacBook-Air-Damian.local>
2019-06-13 18:21:41 +0200

    API Fixes

--- server/src/message/message.module.ts
+++ server/src/message/message.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { MessageSchema } from './schemas/message.schema';
 import { MessageService } from './message.service';
 import { UserModule } from 'src/user/user.module';
+import { MessageController } from './message.controller';
 
 @Module({
   imports: [MongooseModule.forFeature([{name: 'Message', schema: MessageSchema}]), UserModule],
+  controllers: [MessageController],
   providers: [MessageService],
   exports: [MessageService],
 })
